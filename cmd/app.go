b633ddb0909316c44e941c13d90e2ae8c3903cd1
SUETAK <akasirotonbo08451@gmail.com>
2021-06-25 00:56:00 +0900

    [update] input practice

--- cmd/app.go
+++ cmd/app.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	fmt.Println("input....")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	fmt.Println("out put", scanner.Text())
 }
